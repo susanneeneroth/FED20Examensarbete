f539c72736dadee9d5aa7bd7ff79f81008543a08
susanneeneroth <susanne@centimedia.se>
2022-03-16 22:32:18 +0100

    Connecting menu to custom theme

--- packages/exjobb-susanne-theme/src/components/index.js
+++ packages/exjobb-susanne-theme/src/components/index.js
@@ -1,19 +1,86 @@
 import React from "react";
-import { connect } from "frontity";
-import Link from "@frontity/components/link";
+import { connect, Global, css, styled } from "frontity";
+import Nav from "../components/nav";
+import Switch from "@frontity/components/switch";
+import List from "./list";
+import Post from "./post";
+import Page from "./page";
 
 const Root = ({ state }) => {
+  const data = state.source.get(state.router.link);
+
   return (
     <>
-      <h1>Frontity workshop</h1>
-      <p>Current URL: {state.router.link}</p>
-      <nav>
-        <Link link="/">Home</Link>
-        <Link link="/page/2">More posts</Link>
-        <Link link="/about-us">About us</Link>
-      </nav>
+      <Global
+        styles={css`
+          * {
+            margin-top: 0;
+            padding: 0;
+            box-sizing: border-box;
+          }
+          html {
+            font-family: system-ui, Verdana, Arial, sans-serif;
+            margin: 0;
+          }
+          body {
+            margin: 0;
+          }
+          h1 {
+            margin-top: 0;
+          }
+        `}
+      />
+      <Header>
+        <HeaderContent>
+          <h1>Frontity workshop</h1>
+          <p>Current URL: {state.router.link}</p>
+          <Menu>
+            <Nav />
+          </Menu>
+        </HeaderContent>
+      </Header>
+      <main>
+        <Switch>
+          <List when={data.isArchive} />
+          <Post when={data.isPost} />
+          <Page when={data.isPage} />
+        </Switch>
+      </main>
     </>
   );
 };
 
 export default connect(Root);
+
+const Header = styled.header`
+  background-color: #ccc;
+
+  h1 {
+    color: #081521;
+  }
+`;
+
+const HeaderContent = styled.div`
+  max-width: 800px;
+  padding: 2em 1em;
+  margin: 0 auto;
+
+  h2 {
+    margin: 0.5em 0;
+  }
+  p {
+    line-height: 1.25em;
+    margin-bottom: 0.75em;
+  }
+`;
+
+const Menu = styled.nav`
+  display: flex;
+  flex-direction: row;
+  margin-top: 1em;
+  & > a {
+    margin-right: 1em;
+    color: steelblue;
+    text-decoration: none;
+  }
+`;
--- packages/exjobb-susanne-theme/src/index.js
+++ packages/exjobb-susanne-theme/src/index.js
@@ -1,4 +1,8 @@
 import Root from "./components";
+import image from "@frontity/html2react/processors/image";
+import iframe from "@frontity/html2react/processors/iframe";
+import link from "@frontity/html2react/processors/link";
+import menuHandler from "./components/handlers/menu-handler";
 
 const exjobbSusanneTheme = {
   name: "exjobb-susanne-theme",
@@ -6,10 +10,55 @@ const exjobbSusanneTheme = {
     theme: Root,
   },
   state: {
-    theme: {},
+    theme: {
+      autoPrefetch: "in-view",
+      menu: ["primary-menu"],
+      menuUrl: "primary-menu",
+      isMobileMenuOpen: false,
+      featured: {
+        showOnList: false,
+        showOnPost: false,
+      },
+    },
   },
   actions: {
-    theme: {},
+    theme: {
+      toggleMobileMenu: ({ state }) => {
+        state.theme.isMobileMenuOpen = !state.theme.isMobileMenuOpen;
+      },
+      closeMobileMenu: ({ state }) => {
+        state.theme.isMobileMenuOpen = false;
+      },
+      beforeSSR: async ({ state, actions }) => {
+        await actions.source.fetch(`/menu/${state.theme.menuUrl}/`);
+      },
+      beforeCSR: () => {
+        import("webfontloader").then((WebFontLoader) => {
+          WebFontLoader.load({
+            google: {
+              families: [
+                "Pacifico",
+                "Merriweather:400,700:latin-ext",
+                "Sacramento",
+              ],
+            },
+          });
+        });
+      },
+    },
+  },
+  libraries: {
+    html2react: {
+      /**
+       * Add a processor to `html2react` so it processes the `<img>` tags
+       * and internal link inside the content HTML.
+       * You can add your own processors too.
+       */
+      processors: [image, iframe, link],
+    },
+    source: {
+      handlers: [menuHandler],
+    },
   },
 };
 
