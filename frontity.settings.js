a6586799cf0163506c2247c58e1c2040b6dc1915
susanneeneroth <susanne@centimedia.se>
2022-05-24 23:36:44 +0200

    Test of url change

--- frontity.settings.js
+++ frontity.settings.js
@@ -2,7 +2,7 @@ const settings = {
   name: "fed20-exjobb",
   state: {
     frontity: {
-      url: "https://www.susanneeneroth.se/",
+      url: "https://susanneeneroth.se/",
       title: "Susanne Eneroth",
       description: "",
     },
@@ -24,7 +24,7 @@ const settings = {
       name: "@frontity/wp-source",
       state: {
         source: {
-          url: "https://www.susanneeneroth.se/",
+          url: "https://susanneeneroth.se/",
           postTypes: [
             {
               type: "more-work",
